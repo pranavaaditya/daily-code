a0dd447035421e2459b70c78a8751a154d884692
PRANAV AADITYA <160780896+pranavaaditya@users.noreply.github.com>
2025-01-23 23:05:31 +0530

    RUNNING SUM OF 1-d ARRAY
    Input Type and Output Type:
    The function takes a list of integers (nums) and returns a list of integers of the same length.
    
    This approach has a time complexity of O(n), where n is the length of the nums list.
    
    The space complexity is O(1) where , O(1) as the input list is modified directly without requiring extra space.

---
+++ day-7.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        for i in range(1,len(nums)):
+            nums[i]+=nums[i-1]
+        return nums
+    
+
+# Time complexity: O(n)
+# Space complexity: O(1)
